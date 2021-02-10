0209bca9a4f14679243ab3d2fee74cf6ba4f7fb6
sedasimsek <sedasimsekk27@gmail.com>
2021-02-11 00:50:28 +0300

    10 Şubat Ders Sonu

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        //readonly(get) constructar'da set edilebilir, harici edilemez.
+        public Result(bool success, string message):this(success)
+        {
+            Message = message;
+            
+        }
+
+        //overloading
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
