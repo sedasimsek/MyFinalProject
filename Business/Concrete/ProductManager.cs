5e5f00621c73c9c3c20560655134de2fa456dfbb
sedasimsek <sedasimsekk27@gmail.com>
2021-02-18 00:11:40 +0300

    17 Şubat Ders Sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,10 +1,14 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -20,21 +24,18 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
-        
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
+
             //business codes
 
-            if (product.ProductName.Length < 2)
-            {
-                //magic string
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
             _productDal.Add(product);
 
             return new SuccessResult(Messages.ProductAdded);
         }
 
+        
         public IDataResult<List<Product>> GetAll()
         {
             //İş kodları
@@ -47,10 +48,12 @@ namespace Business.Concrete
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductsListed);
         }
 
+        
         public IDataResult<List<Product>> GetAllByCategoryId(int id)
         {
             return new SuccessDataResult<List<Product>> (_productDal.GetAll(p => p.CategoryId == id));
         }
+
 
         public IDataResult<Product> GetById(int productId)
         {
