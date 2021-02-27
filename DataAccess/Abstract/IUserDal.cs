dc957086d5b69e0587721c42f892b1db9f2daf92
sedasimsek <sedasimsekk27@gmail.com>
2021-02-28 01:58:19 +0300

    27 Şubat Der sonu

---
+++ DataAccess/Abstract/IUserDal.cs
@@ -0,0 +1,15 @@
+﻿using Core.Abstract;
+using Core.Entities.Concrete;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IUserDal : IEntityRepository<User>
+    {
+        List<OperationClaim> GetClaims(User user);
+    }
+}
